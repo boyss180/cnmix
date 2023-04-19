15862c69c5c7bf9cce0f56e576092723fc5da35a
boyss180 <83221411+boyss180@users.noreply.github.com>
2023-04-19 12:59:21 +0800

    Update voflix_n.js

--- js2/voflix_n.js
+++ js2/voflix_n.js
@@ -1,6 +1,6 @@
 var rule={
     title:'VOFLIX HD',
-    // host:'https://www.voflix.com',
+    // host:'https://www.voflix.me',
     host:'https://www.voflix.me',
     // homeUrl:'/',
     // url:'/show/fyclass--------fypage---.html',
